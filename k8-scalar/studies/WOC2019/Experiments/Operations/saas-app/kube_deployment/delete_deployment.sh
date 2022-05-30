459cbfaeb6fabcb2b3e93d012d31e7e152b29df0
gbudigiri <60463007+gbudigiri@users.noreply.github.com>
2022-05-30 16:53:25 +0200

    Add files via upload

---
+++ k8-scalar/studies/WOC2019/Experiments/Operations/saas-app/kube_deployment/delete_deployment.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+for sla in `ls slas` 
+  do 
+   namespace=`sed -e 's#.*_\(\)#\1#' <<< ${sla}`
+   echo SLA configuration ${namespace}
+   echo ==============================
+   kubectl delete -f slas/${sla}/limits.yaml --namespace=${namespace}
+   kubectl delete -f deploy_saas_kube.yaml --namespace=${namespace}
+   kubectl delete -f expose.yaml --namespace=${namespace}
+   kubectl delete po --all -n test
+   kubectl delete -f slas/${sla}/namespace.yaml
+   expctrl=$(helm list -aq 2>&1)
+   helm uninstall $expctrl
+  done
+
