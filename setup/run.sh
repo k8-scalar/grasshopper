446ff106731719d6d950d3b1541f62c964a46521
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2024-10-11 17:20:23 +0000

    update

--- setup/run.sh
+++ setup/run.sh
@@ -1,10 +1,10 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.10
+sudo apt install -y python3.10
 python3.10 --version
-sudo apt install python3.10-distutils
+sudo apt install -y python3.10-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3.10 get-pip.py
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
@@ -12,15 +12,15 @@ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 
 sudo update-alternatives --config python3
 sudo update-alternatives --install /usr/bin/pip pip /usr/local/bin/pip3.10 1
 sudo apt update
-sudo apt install python3-dev build-essential
-sudo apt install python3-apt
-sudo apt-get install libffi-dev
-sudo apt install pkg-config
-sudo apt install cmake
-sudo apt install libcairo2 libcairo2-dev
-sudo apt install meson ninja-build
-sudo apt install libgirepository1.0-dev gir1.2-gtk-4.0
+sudo apt install -y python3-dev build-essential
+sudo apt install -y python3-apt
+sudo apt-get install -y libffi-dev
+sudo apt install -y pkg-config
+sudo apt install -y cmake
+sudo apt install -y libcairo2 libcairo2-dev
+sudo apt install -y meson ninja-build
+sudo apt install -y libgirepository1.0-dev gir1.2-gtk-4.0
 pip install --upgrade pip setuptools wheel
-sudo apt-get install libdbus-1-dev libdbus-glib-1-dev
+sudo apt-get install -y libdbus-1-dev libdbus-glib-1-dev
 pip install -r requirements.txt
 echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
