9aebee503138f842d887ec16518404dd5bb6c734
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2024-10-16 14:57:47 +0000

    remove-mount

--- setup/install_kubeadm/1.pre-install-k8s-node/pre-k8s-node.sh
+++ setup/install_kubeadm/1.pre-install-k8s-node/pre-k8s-node.sh
@@ -1,7 +1,3 @@
-export nfs_account=kronos-dnet-hera.cs.kuleuven.be:/prjeddy
-export local_nfs_dir=/mnt/nfs-disk-2/
-./mount-nfs.sh
-
 export containerd_version=1.7.22
 export runc_version=1.1.14
 export nerdctl_version=1.7.7
--- setup/install_kubeadm/run.sh
+++ setup/install_kubeadm/run.sh
@@ -3,8 +3,6 @@ export kubernetes_master_node=172.22.8.143
 export subnetmask=172.22.8
 export nodes="146 28"
 export calico_version=3.28.2
-export nfs_account=kronos-dnet-hera.cs.kuleuven.be:/prjeddy
-export local_nfs_dir=/mnt/nfs-disk-2/
 export containerd_version=1.7.22
 export runc_version=1.1.14
 export nerdctl_version=1.7.7
@@ -14,7 +12,7 @@ sed -i "s/kubernetes_service_host: \".*\"/kubernetes_service_host: \"${kubernete
 sed -i "s/export kubeadm_version=.*/export kubeadm_version=${kubeadm_version}/g" install_kubeadm_worker.sh
 sed -i "s/\"cniVersion\": .*/\"cniVersion\": \"${cni_plugins_version}\"/g"   calico-setup/calico-configmap.yml
 sed -i "s|cidr: .*|cidr: ${pod_cidr}|g"  calico-setup/custom-resources.yaml
-for var in nfs_account local_nfs_dir containerd_version runc_version nerdctl_version cni_plugins_version
+for var in containerd_version runc_version nerdctl_version cni_plugins_version
 do
     sed -i "s|export $var=.*|export $var=${!var}|g" 1.pre-install-k8s-node/pre-k8s-node.sh
 done
