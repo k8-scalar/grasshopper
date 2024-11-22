e31e06dc4b03f1e80fcf089deff4fb61e3c51b1d
Robbe Serry <robbeserry@gmail.com>
2024-11-22 12:23:26 +0100

    add __str__ methods for improved string representation of classes

--- classes.py
+++ classes.py
@@ -4,15 +4,24 @@ class Traffic:
         self.port = port
         self.protocol = protocol
 
+    def __str__(self):
+        return f"Traffic(direction={self.direction}, port={self.port}, protocol={self.protocol})"
+
 
 class LabelSet:
     def __init__(self, labels: dict[str, str]):
         self.labels = labels
 
+    def __str__(self):
+        return f"LabelSet(labels={self.labels})"
+
 
 class CIDR:
     def __init__(self, cidr: str):
         self.cidr = cidr
+
+    def __str__(self):
+        return f"CIDR(cidr={self.cidr})"
 
 
 class Policy:
@@ -23,12 +32,18 @@ class Policy:
         self.sel = sel
         self.allow = allow
 
+    def __str__(self):
+        return f"Policy(name={self.name}, sel={self.sel}, allow={self.allow})"
+
 
 class SecurityGroup:
     def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
         self.remotes: set[Rule] = set()
+
+    def __str__(self):
+        return f"SecurityGroup(id={self.id}, name={self.name}, remotes={self.remotes})"
 
 
 class Pod:
@@ -45,6 +60,11 @@ class Pod:
             return self.name == other.name
         return False
 
+    def __str__(self):
+        return (
+            f"Pod(name={self.name}, label_set={self.label_set}, status={self.status})"
+        )
+
 
 class Rule:
     def __init__(self, target: SecurityGroup | CIDR, traffic: Traffic):
@@ -54,10 +74,16 @@ class Rule:
     def set_Id(self, id: str):
         self.id = id
 
+    def __str__(self):
+        return f"Rule(target={self.target}, traffic={self.traffic})"
+
 
 class Node:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
+
+    def __str__(self):
+        return f"Node(name={self.name})"
 
 
 class MapEntry:
@@ -65,3 +91,9 @@ class MapEntry:
         self.match_nodes: set[Node] = set()
         self.select_pols: set[Policy] = set()
         self.allow_pols: set[Policy] = set()
+
+    def __str__(self):
+        return (
+            f"MapEntry(match_nodes={self.match_nodes}, "
+            f"select_pols={self.select_pols}, allow_pols={self.allow_pols})"
+        )
