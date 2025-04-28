27ffdc30e0fe07d533faa93fb420ab8704e38b57
QuintenLauwaert <quintenlauwaert123@gmail.com>
2025-04-28 08:01:52 +0000

    Changed intervals

--- experiments/system-usage/test/test-burst-full-experiment.sh
+++ experiments/system-usage/test/test-burst-full-experiment.sh
@@ -1,8 +1,6 @@
 
-
-
 num_pods_to_burst=("10" "20" "50" "100" "200" "500")
-intervals=("0.5" "1" "1" "2" "2" "2" "2")
+intervals=("0.5" "1" "1" "1" "1" "1")
 
 # Loop through both lists using an index
 for i in "${!num_pods_to_burst[@]}"; do
