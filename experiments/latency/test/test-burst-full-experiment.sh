34d598afeb788c0b0fe1fb5d5e19c8c89ff24d38
QuintenLauwaert <quintenlauwaert123@gmail.com>
2025-04-29 09:31:14 +0000

    Implemented full experiments (for creation times)

--- experiments/latency/test/test-burst-full-experiment.sh
+++ experiments/latency/test/test-burst-full-experiment.sh
@@ -1,11 +1,12 @@
 
-num_pods_to_burst=("10" "20" "50" "100" "200" "500")
-intervals=("0.5" "1" "1" "1" "1" "1")
+# num_pods_to_burst=("10" "20" "50" "100" "200" "500")
+num_pods_to_burst=("10" "20" "50")
+iterations=("1" "1" "1")
 
 # Loop through both lists using an index
 for i in "${!num_pods_to_burst[@]}"; do
     num_pods="${num_pods_to_burst[$i]}"
-    interval="${intervals[$i]}"
-    echo "Doing experiment for $num_pods pods, with interval of $interval seconds"
-    ./test/test-burst-iterations.sh $num_pods $interval
+    iteration="${iterations[$i]}"
+    echo "Doing experiment for $num_pods pods, with $iteration iterations."
+    ./test/test-burst-iterations.sh $num_pods $iteration
 done
