a4756bf94b1649d0ad660350b865fa6032d66ad9
eddy truyen <eddy.truyen@cs.kuleuven.be>
2025-05-21 16:21:51 +0000

    update

--- setup/install_kubeadm/run.sh
+++ setup/install_kubeadm/run.sh
@@ -1,7 +1,7 @@
 export kubeadm_version=1.31
-export kubernetes_master_node=172.22.8.143
-export subnetmask=172.22.8
-export nodes="146 28"
+export kubernetes_master_node=172.23.24.76
+export subnetmask=172.23.24
+export nodes="23 63 116"
 export calico_version=3.28.2
 export containerd_version=1.7.22
 export runc_version=1.1.14
@@ -11,6 +11,7 @@ export pod_cidr=192.168.0.0/16
 sed -i "s/kubernetes_service_host: \".*\"/kubernetes_service_host: \"${kubernetes_master_node}\"/g" calico-setup/calico-configmap.yml
 sed -i "s/export kubeadm_version=.*/export kubeadm_version=${kubeadm_version}/g" install_kubeadm_worker.sh
 sed -i "s/\"cniVersion\": .*/\"cniVersion\": \"${cni_plugins_version}\"/g"   calico-setup/calico-configmap.yml
+sed -i "s/KUBERNETES_SERVICE_HOST: .*/KUBERNETES_SERVICE_HOST: \'${kubernetes_master_node}\'/g" calico-setup/k8s-apiserver-cm.yml
 sed -i "s|cidr: .*|cidr: ${pod_cidr}|g"  calico-setup/custom-resources.yaml
 for var in containerd_version runc_version nerdctl_version cni_plugins_version
 do
